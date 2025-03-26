8c332cd8ff1e2c68e332741781c11538265e4654
Sullivan-devweb <dewinnesullivan10@gmail.com>
2025-03-26 15:37:13 +0100

    Update politique.php

--- politique.php
+++ politique.php
@@ -3,9 +3,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Politique de Confidentialité</title>
-    <link rel="stylesheet" href="politique.css">
+    <link rel="stylesheet" href="css/politique.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="css/footer.css">
 </head>
 <body>
     <header>
