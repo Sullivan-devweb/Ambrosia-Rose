6f218a501ad73c2d5b4b162950d22dad8aefea10
Sullivan-devweb <dewinnesullivan10@gmail.com>
2025-03-26 15:21:43 +0100

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -101,10 +101,10 @@ $utilisateurs = $stmtUtilisateurs->fetchAll(PDO::FETCH_ASSOC);
 
     <!-- Styles CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="accueil.css">
-    <link rel="stylesheet" href="nav.css">
-    <link rel="stylesheet" href="footer.css">
-    <link rel="stylesheet" href="badge_premium.css">
+    <link rel="stylesheet" href="css/accueil.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/badge_premium.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
