5f88b87a0fbd3577afe28425b43f3191ba455e51
Sullivan-devweb <dewinnesullivan10@gmail.com>
2025-03-26 15:31:19 +0100

    Update editprofil.php

--- editprofil.php
+++ editprofil.php
@@ -69,9 +69,9 @@ if (
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Éditer le Profil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="editprofil.css">
-    <link rel="stylesheet" href="nav.css">
-    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="css/editprofil.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet">
