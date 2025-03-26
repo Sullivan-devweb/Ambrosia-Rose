25c8e5012bb273e56dd6d1a0a291f816b40818d2
Sullivan-devweb <dewinnesullivan10@gmail.com>
2025-03-26 15:33:34 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -6,7 +6,7 @@
     <title>Inscription</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="inscription.css">
+    <link rel="stylesheet" href="css/inscription.css">
     <style>
         .hidden { display: none; }
         .error { border: 1px solid red; }
@@ -171,7 +171,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="inscription.js"></script>
+    <script src="js/inscription.js"></script>
     <script>
         $(document).ready(function () {
             let currentStep = 1;
